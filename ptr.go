53794727c2576635e142c4a1f0a1a45ad238e966
Denis Chevalier <perso@denischevalier.fr>
2025-07-04 21:16:43 +0200

    some change

--- ptr.go
+++ ptr.go
@@ -4,12 +4,3 @@ package ptr
 func New[T any](value T) *T {
 	return &value
 }
-
-// Value returns the underlying value of a pointer
-func Value[T any](ptr *T) T {
-	if ptr == nil {
-		var ret T
-		return ret
-	}
-	return *ptr
-}
